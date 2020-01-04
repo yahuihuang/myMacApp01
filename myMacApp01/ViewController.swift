a612cf83b359eb4d9fdd39549cc83096a3e871fd
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 17:08:17 +0800

    use NSOpenPanel

--- myMacApp01/ViewController.swift
+++ myMacApp01/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import Cocoa
+import AVFoundation
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDelegate {
 
+    @IBOutlet weak var textField: NSTextField!
+    let synth = AVSpeechSynthesizer()
+    
+    @IBOutlet weak var playlistTableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,5 +28,32 @@ class ViewController: NSViewController {
     }
 
 
+    @IBAction func buttonClickAction(_ sender: NSButton) {
+//        print("button Click !")
+//
+//        var text = textField.stringValue
+//        if text == "" {
+//            text = "寫些字吧!"
+//        }
+//
+//        let myUtterance = AVSpeechUtterance(string: text)
+//        myUtterance.voice = AVSpeechSynthesisVoice(language: "zh-tw")
+//        myUtterance.rate = 0.1
+//        synth.speak(myUtterance)
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["mp3", "plist"]
+        openPanel.beginSheetModal(for: self.view.window!) { (response) in
+            print(openPanel.url?.lastPathComponent)
+            self.textField.stringValue = openPanel.url!.lastPathComponent
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 2;
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        
+    }
 }
 
