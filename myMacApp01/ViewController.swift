489a6beac47beb27a7d8487a47aecf4f8aa35c7b
Huang Grace <huang.yahui.grace@gmail.com>
2020-01-04 16:11:04 +0800

    Initial Commit

---
+++ myMacApp01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myMacApp01
+//
+//  Created by grace on 2020/1/4.
+//  Copyright © 2020 grace. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
